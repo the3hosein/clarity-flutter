cdd3f9e2c113c243c88335e975d709428cbb987a
the3hosein <s.h.mosavi5653.5653@gmail.com>
2026-06-15 22:09:36 +0330

    Remove unused glass_card import in empty_state

--- lib/widgets/empty_state.dart
+++ lib/widgets/empty_state.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'glass_card.dart';
 
 class EmptyState extends StatelessWidget {
   final IconData icon;
